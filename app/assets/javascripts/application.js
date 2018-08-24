3587f7d7c3ddb435e95458708fd47e2459d06510
wichru <wicher.maciej@gmail.com>
2018-08-24 12:40:45 +0200

    update bootstrap error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
+
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
