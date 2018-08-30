31e3846bd13c977a590e1cadd219760b77d26a09
wichru <wicher.maciej@gmail.com>
2018-08-31 01:00:46 +0200

    working RSpec test

--- spec/models/citation_spec.rb
+++ spec/models/citation_spec.rb
@@ -1,12 +1,9 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe Citation, type: :model do
   it 'is valid with valid attributes' do
-    expect(Citation.new).to be_valid
+    expect(Citation.new(text: 'anything')).to be_valid
   end
-
-  it 'is not valid without a title'
-  it 'is not valid without a added_date'
 
   it 'is not valid without a text' do
     citation = Citation.new(text: nil)
